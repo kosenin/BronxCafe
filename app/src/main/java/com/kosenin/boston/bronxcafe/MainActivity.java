8c72a797cb5cbb7f9c2a831ed59c5baceb9b6770
kosenin <kosenin@yandex.ru>
2018-05-07 23:03:24 +0300

    Just to synchronize

--- app/src/main/java/com/kosenin/boston/bronxcafe/MainActivity.java
+++ app/src/main/java/com/kosenin/boston/bronxcafe/MainActivity.java
@@ -3,12 +3,15 @@ package com.kosenin.boston.bronxcafe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.backendless.Backendless;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 import com.backendless.persistence.BackendlessDataQuery;
 import com.backendless.persistence.DataQueryBuilder;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,8 @@ import static com.kosenin.boston.bronxcafe.BackendlessSettings.APPLICATIONID;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Food> foodList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,21 +30,39 @@ public class MainActivity extends AppCompatActivity {
 
         Backendless.initApp(getApplicationContext(), APPLICATIONID, ANDROIDAPIKEY);
 
-        List<Food> foodList=new ArrayList<>();
+
         DataQueryBuilder queryBuilder = DataQueryBuilder.create();
         queryBuilder.setWhereClause("type = 'roll'");
 
         Backendless.Data.of(Food.class).find(queryBuilder, new AsyncCallback<List<Food>>() {
             @Override
             public void handleResponse(List<Food> response) {
-                Log.e("Backendless Response..","no of object.."+response.size());
+
+
+                //TODO DB cache of response
+
+
+                TextView textView1 = findViewById(R.id.name);
+                TextView textView2 = findViewById(R.id.description);
+                ImageView imageView1 = findViewById(R.id.picture);
+
+
+                textView1.setText(response.get(0).getName());
+                textView2.setText(response.get(0).getDescription());
+                Picasso.get().load(response.get(0).getPicture()).into(imageView1);
+
+                Log.e("Backendless Response..", "response.." + response.size());
+                Log.e("Backendless Response..", "foodlist.." + foodList.size());
             }
+
 
             @Override
             public void handleFault(BackendlessFault fault) {
-                Log.e("Backendless Response..","FUCK");
+                Log.e("Backendless Response..", "FUCK");
             }
         });
 
     }
+
+
 }
---
+++ app/src/main/java/com/kosenin/boston/bronxcafe/SandwichesActivity.java
@@ -0,0 +1,13 @@
+package com.kosenin.boston.bronxcafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SandwichesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sandwiches);
+    }
+}
