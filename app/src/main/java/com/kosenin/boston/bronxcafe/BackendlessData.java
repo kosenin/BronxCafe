4188e290a4a9271325f5d4323c8bde5daf40a7bc
kosenin <kosenin@yandex.ru>
2018-05-20 22:17:18 +0300

    BackendlessData in activity

--- app/src/main/java/com/kosenin/boston/bronxcafe/BackendlessData.java
+++ app/src/main/java/com/kosenin/boston/bronxcafe/BackendlessData.java
@@ -1,28 +1,50 @@
 package com.kosenin.boston.bronxcafe;
 
+import android.app.Activity;
 import android.app.Application;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.backendless.Backendless;
+import com.backendless.persistence.DataQueryBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static com.kosenin.boston.bronxcafe.BackendlessSettings.ANDROIDAPIKEY;
 import static com.kosenin.boston.bronxcafe.BackendlessSettings.APPLICATIONID;
 
-public class BackendlessData {
 
-    public static void BackendlessInit(Context context)
-    {
+public class BackendlessData extends AsyncTask {
+
+    List<Food> foodList = new ArrayList<>();
+
+    public static final String TYPE_ROLL = "type = 'roll'";
+    public static final String TYPE_SANDWICH = "type = 'sandwich'";
+    public static final String TYPE_DRINK = "type = 'drink'";
+
+
+    public static void BackendlessInit(Context context) {
         Backendless.initApp(context, APPLICATIONID, ANDROIDAPIKEY);
     }
 
-    public static List getBackendlessData(String queryClause, List list)
-    {
-        if (queryClause==null)
-        {
+    @Override
+    protected List<Food> doInBackground(Object[] objects) {
 
-        }
-        return list;
+
+        DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+        queryBuilder.setWhereClause(TYPE_ROLL);
+
+        foodList = Backendless.Data.of(Food.class).find(queryBuilder);
+      //  Log.e("BackendlessAsync", String.valueOf(foodList.size()));
+
+        return foodList;
     }
+
+
 }
+
+
--- app/src/main/java/com/kosenin/boston/bronxcafe/MainActivity.java
+++ app/src/main/java/com/kosenin/boston/bronxcafe/MainActivity.java
@@ -15,52 +15,34 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static com.kosenin.boston.bronxcafe.BackendlessSettings.ANDROIDAPIKEY;
 import static com.kosenin.boston.bronxcafe.BackendlessSettings.APPLICATIONID;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Food> foodList = new ArrayList<>();
+    List<Food> foodDataList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         BackendlessData.BackendlessInit(getApplicationContext());
 
-
-        DataQueryBuilder queryBuilder = DataQueryBuilder.create();
-        queryBuilder.setWhereClause("type = 'roll'");
-
-        Backendless.Data.of(Food.class).find(queryBuilder, new AsyncCallback<List<Food>>() {
-            @Override
-            public void handleResponse(List<Food> response) {
-
-
-                //TODO DB cache of response
+        try {
+            foodDataList = (List) new BackendlessData().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
 
-                TextView textView1 = findViewById(R.id.name);
-                TextView textView2 = findViewById(R.id.description);
-                ImageView imageView1 = findViewById(R.id.picture);
-
-
-                textView1.setText(response.get(0).getName());
-                textView2.setText(response.get(0).getDescription());
-                Picasso.get().load(response.get(0).getPicture()).into(imageView1);
-
-                Log.e("Backendless Response..", "response.." + response.size());
-                Log.e("Backendless Response..", "foodlist.." + foodList.size());
-            }
-
-
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                Log.e("Backendless Response..", "FUCK");
-            }
-        });
+        Log.e("BackendlessActivity", String.valueOf(foodDataList.size()));
 
     }
 
